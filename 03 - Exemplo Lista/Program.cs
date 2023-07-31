167c6d8e766c53176a3c067123762552f5c7a8ba
Sthefany <s532401ed@gmail.com>
2023-07-31 16:44:57 -0400

    first commit

---
+++ 03 - Exemplo Lista/Program.cs
@@ -0,0 +1,16 @@
+﻿using _03___Exemplo_Lista;
+
+Aluno aluno1 = new Aluno();
+aluno1.Nome = "Elias";
+aluno1.Cpf = "222";
+
+Aluno aluno2 = new Aluno(11, "Maria", "05364291174", new DateOnly(1995, 07, 23));
+
+List<Aluno> alunos = new List<Aluno>();
+alunos.Add(aluno1);
+alunos.Add(aluno2);
+
+foreach(Aluno a in alunos)
+{
+    Console.WriteLine($"O nome é {a.Nome} {a.Cpf}");
+}
